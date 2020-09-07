d65a648fc1c3b29fb0915d39c1faa291e5c2ab58
Ivan Danilov <ivandanilov996@yandex.ru>
2020-09-07 17:27:18 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,5 @@
 // TODO: write your code here
-console.log("app worked");
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js'
+
+const game = new Game();
+game.start();
